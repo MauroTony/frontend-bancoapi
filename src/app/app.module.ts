2241fd1b5191107cb33df1860aa25da472f13f67
Mauro Tony <57079165+MauroTony@users.noreply.github.com>
2022-08-05 04:19:50 -0300

    Configurando Modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { ContaModule, ContaRoutingModule } from './conta';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    ContaModule,
+    ContaRoutingModule,
+
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/conta/index.ts
@@ -0,0 +1,3 @@
+export * from './components';
+export * from './conta.module';
+export * from './conta-routing.module';
